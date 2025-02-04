3f092d61fab4921b44da3b63cdab873eec96d20a
Nuttapon <64110008@kmitl.ac.th>
2025-02-04 11:34:21 +0700

    ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [$# -eq 0]; then
- echo "No argument supplied"
-else
- for i in $(seq 1 3); do
-	 if [ $i -le $# ] then
-		 echo "${!i}"
-	  fi
-  done
+if [ $# -eq 0 ]; then
+	echo "No argument supplied"
+else 
+	for i in $(seq 1 3); do
+		if [ $i -le $# ]; then
+			echo "${!i}"
+		fi
+	done
 fi
